ad7363726116e6d30491df19449a8dddf4b1e9a8
kinkard <stsiapan.kizim@gmail.com>
2026-04-16 16:37:31 +0200

    feat: tokio + axum basic settup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use std::num::NonZero;
+
+use axum::{Router, routing::get};
+use clap::Parser;
+use tokio::signal;
+use tracing::info;
+
+#[derive(Parser)]
+struct Config {
+    /// Path to the archive file on local disk or on S3 (with "s3" feature enabled and with `s3://` prefix).
+    archive: String,
+    /// Port to listen
+    #[arg(long, default_value_t = 3000)]
+    port: u16,
+    /// Max threads to use
+    #[arg(long, default_value_t = 4)]
+    concurrency: u16,
+}
+
+#[derive(Clone)]
+struct AppState {}
+
 fn main() {
-    println!("Hello World, Rati!");
+    tracing_subscriber::fmt::init();
+
+    let config = Config::parse();
+
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(
+            std::thread::available_parallelism()
+                .map(NonZero::get)
+                .unwrap_or(16) // fallback to 16 as max if we can't get the number of CPUs
+                .min(config.concurrency as usize),
+        )
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(run(config))
+}
+
+async fn run(config: Config) {
+    let app = Router::new()
+        .route("/health", get(|| async { "OK" })) // simple health check endpoint
+        .with_state(AppState {});
+
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", config.port))
+        .await
+        .unwrap();
+    info!("Listening at http://localhost:{}", config.port);
+    axum::serve(listener, app)
+        .with_graceful_shutdown(async {
+            tokio::select! {
+                _ = signal::ctrl_c() => {
+                    info!("Ctrl+C received, shutting down");
+                }
+                _ = async {
+                    signal::unix::signal(signal::unix::SignalKind::terminate())
+                        .expect("failed to install SIGTERM signal handler")
+                        .recv()
+                        .await
+                } => {
+                    info!("SIGTERM received, shutting down");
+                }
+            }
+        })
+        .await
+        .unwrap();
 }
